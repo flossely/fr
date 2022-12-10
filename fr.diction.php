afae15ecb0da021d08dd94cdfcbd741690c02a1b
Karl August Nestor <jesuscaesar@outlook.com>
2022-12-10 13:18:39 +1000

    Latest update

---
+++ fr.diction.php
@@ -0,0 +1,63 @@
+<?php
+
+$diction['fr'] =
+[
+    'digital' =>
+    [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9'
+    ],
+    'alphabet' =>
+    [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',            
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z'
+    ],
+    'action' =>
+    [
+        '100' => 'gagné',
+        '200' => 'tour terminé',
+        '300' => 'ne peut pas accéder',
+        '400' => 'ne peut pas atteindre',
+        '500' => 'est mort'
+    ],
+    'transaction' =>
+    [
+        '00' => 'TRANSACTION_APPROUVÉE',
+        '12' => 'TRANSACTION_INVALIDE',
+        '30' => 'ERREUR_DE_FORMAT',
+        '51' => 'FONDS_INSUFFISANTS'
+    ],
+];
+
